e0b8f9d828db49e29ff34193203c756b490ae1d8
Mac <mac@Macs-MacBook-Air.local>
2026-08-02 13:48:44 +0500

    Fix Render port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const webhook = require("./routes/webhook");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use("/webhook", webhook);
 app.get("/", (req, res) => {
   res.send("Infinity AI WhatsApp Bot is Running!");
